3c49d4783095b0d2f1749e481a997315aed5acff
dlugi57 <dlugoszpeter@gmail.com>
2020-04-15 22:32:46 +0200

    Create symptom parser class and use reader in proper way

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,35 +3,24 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
-	
+
+	private static String filePath = "symptoms.txt";
+
 	public static void main(String args[]) throws Exception {
-		// first get input test of commit to another brunch
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
 
-			line = reader.readLine();	// get another symptom
-		}
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile(filePath);
+		List<String> symptomList = reader.GetSymptoms();
+
+		SymptomParser symptoms = new SymptomParser(symptomList);
+
+
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomParser.java
@@ -0,0 +1,13 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+
+public class SymptomParser {
+    private List<String> symptoms;
+
+    public SymptomParser(List symptoms) {
+        this.symptoms = symptoms;
+    }
+
+
+}
