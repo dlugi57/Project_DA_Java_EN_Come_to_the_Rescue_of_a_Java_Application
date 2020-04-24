c05a8220993b917a896297b8ccefbe7ab0433e8a
dlugi57 <dlugoszpeter@gmail.com>
2020-04-24 23:31:57 +0200

    Clean

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -29,7 +29,7 @@ public class AnalyticsCounter {
             ISymptomWriter writer = new SymptomDataToFileWriter(writerFilePath);
             writer.writeSymptoms(symptomsMap);
         } catch (IOException | IllegalStateException e) {
-            logger.log(Level.WARNING, "AnalyticsCounter find an error: " + e);
+            logger.log(Level.WARNING, "AnalyticsCounter found an error: " + e);
             e.printStackTrace();
         }
     }
